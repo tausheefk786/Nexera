1403b504707eedcacf552d95fcf16286e5bf990d
k786 <tausheefk786@gmail.com>
2025-11-03 23:24:48 +0530

    Implement user authenticatio

---
+++ server/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import userRoute from "./routes/user.js"
+
+const app = express();
+
+app.use("/user",userRoute);
+
+app.get("/",(req,res)=>{
+    res.send("hello world lund mera")
+})
+
+app.listen(3000, ()=>{
+    console.log("server is running");
+})
