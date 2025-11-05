6c41466ec6bcc5ef47aa3ff89f8acc5f6698f40c
k786 <tausheefk786@gmail.com>
2025-11-05 23:33:40 +0530

    add message model

--- server/models/message.js
+++ server/models/message.js
@@ -2,31 +2,38 @@ import { Schema,model,models } from "mongoose";
 
 
 const schema = new Schema({
-    name:{
-        type: String,
-        required: true,
-    },
-    username: {
-        type:String,
-        required: true,
-        unique: true,
-    },
-    password:{
-        type: String,
-        required: true,
-        select: false,  
-    },
-    avatar: {
-       public_id: {
-        type: String,
-        required: true,
-       } ,
-        url: {
-        type: String,
-        required: true,
-       } ,
-    }
-},
-{timestamps: true})
+   content: String,
 
-export const User = models.User || model("User",schema);
+attachments: [
+  {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+],
+
+sender: {
+  type: Types.ObjectId,
+  ref: "User",
+  required: true,
+},
+
+chat: {
+  type: Types.ObjectId,
+  ref: "Chat",
+  required: true,
+},
+},
+{
+  timestamps: true,
+}
+);
+
+
+
+export const Message = models.Message || model("Message",schema);
