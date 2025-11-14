16055763c647e6fdfdc6fb283dcd4b50a92fa2ad
k786 <tausheefk786@gmail.com>
2025-11-14 23:13:40 +0530

    Refactor newUser function

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,9 @@ import { User } from "../models/user.js";
 
 const newUser = async (req,res)=>{
 
-    const {name,username,password,bio} = req.body;
+const { name, username, password, bio } = req.body && Object.keys(req.body).length ? req.body : req.query;
+
+
 
     console.log(req.body);
     const avatar = {
