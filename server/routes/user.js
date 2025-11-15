43fbef093d58c36c12accb1ea44c9025028e0cde
k786 <tausheefk786@gmail.com>
2025-11-15 21:15:27 +0530

    Fix formatting

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { login, newUser } from "../controllers/user.js";
 
+
 import { singleAvatar } from "../middlewares/multer.js";
 const app = express.Router();
 
 app.post("/new",singleAvatar,newUser);
 app.post("/login",login);
-
 
 export default app;
