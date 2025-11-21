5c979ba0fa47d88cda76a69cf7e4b3d11da9f349
k786 <tausheefk786@gmail.com>
2025-11-21 23:58:34 +0530

    checkd login function using postman

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { User } from "../models/user.js";
 import { sendToken } from "../utils/features.js";
 
+
 const newUser = async (req,res)=>{
 const { name, username, password, bio } = req.body && Object.keys(req.body).length ? req.body : req.query;
     console.log(req.body);
@@ -24,12 +25,12 @@ const login =  async (req,res)=>{
     const {username ,password} = req.body;
     const user = await User.findOne({username}).select("+password");
     if(!user) {
-        return res.status(400).json({message:"Invalid credentials"})
+        return res.status(400).json({message:"Invalid Username"})
     }
     const isMatch = await compare(password,user.password);
 
     if(!isMatch) {
-        return res.status(400).json({message:"Invalid credentials"})
+        return res.status(400).json({message:"Invalid Password"})
     }
     sendToken(res,user,200,`Welcome back, ${user.name}`);
 }
